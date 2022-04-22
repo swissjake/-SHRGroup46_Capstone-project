96d9a7f704fc31234bfd077d504c7f62a39aeea2
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2022-04-22 19:37:05 +0100

    updated project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import HomePage from './components/homePage/HomePage'
+import NavBar from './components/navBar/NavBar'
+
+
+function App() {
+  return (
+    <>
+     <NavBar />
+
+    <HomePage />
+   
+      {/*
+
+ <NavBar />
+   <AboutPage/>
+    <ProductPage/> */}
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './home.css'
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <div className="hero-container">
+          <div className="hero-text">
+              <h1>Welcome to the <strong id='BrownWord'>home</strong> of uncompromising <em id='BrownWord'>Variety</em> and <em id='BrownWord'>Quality</em> in cuisine</h1>
+              <button>Order Now</button>
+          </div>
+            </div>
+    )
+  }
+}
