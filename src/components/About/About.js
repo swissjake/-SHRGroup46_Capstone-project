2fb1af78b32cf4eef547ae1ee53639e5afb7507e
johnpaul <edehjohnpaul@gmail.com>
2022-04-23 13:06:32 -0700

    Update About.js

---
+++ src/components/About/About.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import FoodDeckLogo from '../../assets/FoodDeckLogo.png'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import './nav.css'
+
+ const NavBar = () => {
+   const[click, setClick] = useState(true);
+
+      const handleToggle = () =>{
+        setClick(prevClick => !prevClick)
+      }
+
+    return (
+    <nav>
+      <div className="navbar-container">
+        <div className="nav-img">
+          <img src={FoodDeckLogo} alt="The Food Deck Logo" />
+        </div>
+        <ul className={!click ? "nav-ul" : "nav-close"}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <li>Contact</li>
+        </ul>
+          <div onClick={handleToggle} className="nav-icons">
+            {click ? <AiOutlineMenu size={30}/> : <AiOutlineClose size={30} />}
+          </div>
+      </div>
+    </nav>
+    )
+  }
+
+
+export default NavBar
