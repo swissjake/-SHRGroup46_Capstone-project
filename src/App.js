d333da84756db5657c1d49e1a3d10d591e11e6c9
Amgbara Sofiritari <38821635+swissjake@users.noreply.github.com>
2022-04-25 22:52:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Hero from './components/hero/Hero'
 import NavBar from './components/navBar/NavBar'
-import About from './components/about/About'
+import About from './components/About/About'
 import Product from './components/products/Product';
 
 
