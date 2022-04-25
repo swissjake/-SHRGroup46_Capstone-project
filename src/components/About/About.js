4d4d276da6ee149663520ee38e18b21864f58609
Amgbara Sofiritari <38821635+swissjake@users.noreply.github.com>
2022-04-25 23:07:35 +0100

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,34 +1,9 @@
-import React, { useState } from 'react'
-import FoodDeckLogo from '../../assets/FoodDeckLogo.png'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import React from 'react'
 import './About.css'
 import FoodDeckImage from '../../assets/FoodDeckImage.jpg'
 
  const About = () => {
-   const[click, setClick] = useState(true);
-
-      const handleToggle = () =>{
-        setClick(prevClick => !prevClick)
-      }
-
-    return (
-    <>
-      <nav>
-        <div className="navbar-container">
-          <div className="nav-img">
-            <img src={FoodDeckLogo} alt="The Food Deck Logo" />
-          </div>
-          <ul className={!click ? "nav-ul" : "nav-close"}>
-            <li>Home</li>
-            <li>About</li>
-            <li>Products</li>
-            <li>Contact</li>
-          </ul>
-          <div onClick={handleToggle} className="nav-icons">
-            {click ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
-          </div>
-        </div>
-      </nav>
+   
       <section className="about">
         <div className="row">
           <h1>About Food deck</h1>
