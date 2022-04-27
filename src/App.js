2798e5371e6eda0902ba694bb023e0ff2c71016a
Gbolahan <ogunrinadegbolahan2000@gmail.com>
2022-04-27 16:17:14 +0100

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import Hero from './components/hero/Hero'
-import NavBar from './components/navBar/NavBar'
-import About from './components/About/About'
-import Product from './components/products/Product';
-import Contact from "./components/contactPage/Contact"
+/** @format */
 
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import NavBar from "./components/navBar/NavBar";
+import About from "./components/About/About";
+import Product from "./components/products/Product";
+import Contact from "./components/contactPage/Contact";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-     <NavBar />
-    <Hero />
-    <Product />
-    <Contact/>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Hero />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
       {/*
     <Hero />
   <About />
@@ -26,7 +31,6 @@ function App() {
  <NavBar />
    <AboutPage/>
     <ProductPage/> */}
-
     </>
   );
 }
